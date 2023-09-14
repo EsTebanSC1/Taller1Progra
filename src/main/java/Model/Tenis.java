5dee051e4774262d8c652431ef1df251ec1dc6dd
EsTebanSC1 <estebanseron@gmail.com>
2023-09-14 09:20:40 -0300

    commit 2

---
+++ src/main/java/Model/Tenis.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Tenis extends Deporte{
+    private String superficie;
+    private String tipopartido;
+    public Tenis(String nombre) {
+        super(nombre);
+        this.superficie=superficie;
+        this.tipopartido=tipopartido;
+    }
+
+    public String getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(String superficie) {
+        this.superficie = superficie;
+    }
+
+    public String getTipopartido() {
+        return tipopartido;
+    }
+
+    public void setTipopartido(String tipopartido) {
+        this.tipopartido = tipopartido;
+    }
+
+    @Override
+    public String toString() {
+        return "Tenis{" +
+                "superficie='" + superficie + '\'' +
+                ", tipopartido='" + tipopartido + '\'' +
+                '}';
+    }
+}
--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package Principal;
 
 public class Main {
     public static void main(String[] args) {
